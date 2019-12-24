18760859e66e7b69f0c3eff6664d19c3e0159f85
Petar Staynov <petar.staynov@gmail.com>
2019-12-24 22:02:34 +0200

    Basic styled homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Homepage from "./components/Homepage/Homepage";
 
 function App() {
     return (
         <div>
-            <h1>Hello world</h1>
+            <Homepage/>
         </div>
     );
 }
