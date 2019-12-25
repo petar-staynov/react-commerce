58791e0bfda98749162122c600e29fa272a92cdc
Petar Staynov <petar.staynov@gmail.com>
2019-12-25 13:14:06 +0200

    MenuItem styling

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './MenuItem.scss';
 
 function MenuItem(props) {
+    const backgroundImageStyle = {
+        backgroundImage: `url(${props.imageUrl })`,
+    };
+
+    const menuItemClassName= `menu-item ${props.size}`;
+
     const title = props.title.charAt(0).toUpperCase() + props.title.slice(1);
     return (
-        <div className='menu-item'>
+        <div className={menuItemClassName} >
+            <div className='background-image' style={backgroundImageStyle}></div>
             <div className='content'>
                 <h1 className='title'>{title}</h1>
                 <span className='subtitle'>SHOP NOW</span>
@@ -12,5 +19,7 @@ function MenuItem(props) {
         </div>
     )
 };
+
+
 
 export default MenuItem;
