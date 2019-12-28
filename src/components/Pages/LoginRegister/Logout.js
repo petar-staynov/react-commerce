f9f8bc3ec0b4b6548605e2aa4d086539f0d029cc
Petar Staynov <petar.staynov@gmail.com>
2019-12-28 16:44:04 +0200

    Login/Register redirect to index on success

--- src/components/Pages/LoginRegister/Logout.js
+++ src/components/Pages/LoginRegister/Logout.js
@@ -2,9 +2,6 @@ import React from 'react';
 import {auth, createUserProfileDocument} from "../../../firebase/firebase.utils";
 
 const Logout = (props) => {
-    console.log('logout');
-
-
     auth.signOut().then(r => {
         alert('Signed out successfully');
         props.history.push('/')
