bf1f2b7025a6ce1002d1e548739ea82cfe63e4a4
Petar Staynov <petar.staynov@gmail.com>
2020-01-10 15:21:14 +0200

    enabled logger for development enviorment

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import {createStore, applyMiddleware, compose} from "redux";
-// import logger from 'redux-logger'
+import logger from 'redux-logger'
 import rootReducer from "./rootReducer";
 import {persistStore} from "redux-persist";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const middlewares = []; //insert logger here
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)
+}
 
 export const store = createStore(
     rootReducer,
