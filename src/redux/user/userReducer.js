194eb3d1e2a95870fadc7f838ca32e951e7cef14
Petar Staynov <petar.staynov@gmail.com>
2020-01-30 16:12:18 +0200

    Fixed sign in not updating state

--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -1,21 +1,19 @@
 import UserActionTypes from "./userActionTypes";
 
 const INITIAL_STATE = {
-    currentUser: null,
+    user: null,
     error: null,
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case UserActionTypes.GOOGLE_SIGN_IN_SUCCESS:
-        case UserActionTypes.EMAIL_SIGN_IN_SUCCESS:
+        case UserActionTypes.SIGN_IN_SUCCESS:
             return {
                 ...state,
                 currentUser: action.payload,
                 error: null,
             };
-        case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
-        case UserActionTypes.EMAIL_SIGN_IN_FAILURE:
+        case UserActionTypes.SIGN_IN_FAILURE:
             return {
                 ...state,
                 error: action.payload,
